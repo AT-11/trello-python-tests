e282c434c6bcabb12a46a8223a4da179b21140c2
Admin <alan.escalera@fundacion-jala.org>
2020-01-28 14:29:22 -0400

    added configurations files

---
+++ core/ui/utils/ConfigurationAuthentication.py
@@ -0,0 +1,13 @@
+
+import json
+
+
+class ConfigurationAuthentication(object):
+    config = ""
+
+    def __init__(self):
+        with open('config.json', 'r') as file:
+            self.config = json.load(file)
+
+    def get_config_file(self):
+        return self.config
