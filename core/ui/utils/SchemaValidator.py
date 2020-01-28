a4358cabff01d219e0f9a2067a46450d990d86fa
juanMartinez <juan.martinez@fundacion-jala.org>
2020-01-28 11:25:49 -0400

    Schema Validator class implemented

---
+++ core/ui/utils/SchemaValidator.py
@@ -0,0 +1,23 @@
+import json
+from jsonschema import validate
+
+
+class SchemaValidator(object):
+
+    # Validates json schema
+    def validate_schema(self, input_value):
+        try:
+            json_schema = {
+                "type": "object",
+                "properties": {
+                    "id": {"type": "string", "pattern": "^[A-Za-z0-9]+$"},
+                    "name": {"type": "string", "pattern": "^[A-Za-z0-9]+$"},
+                    "desc": {"type": "string", "pattern": "^$|^[A-Za-z0-9]+$"},
+                }
+            }
+            my_json = json.loads(input_value)
+            validate(instance=my_json, schema=json_schema)
+            print(my_json)
+            return True
+        except:
+            return False
