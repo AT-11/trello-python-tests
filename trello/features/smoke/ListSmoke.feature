512e5f27150c734465d724b17db470c845a3de1d
juanMartinez <juan.martinez@fundacion-jala.org>
2020-01-28 14:18:02 -0400

    obsevations fixed of list steps

--- trello/features/smoke/ListSmoke.feature
+++ trello/features/smoke/ListSmoke.feature
@@ -1,26 +1,22 @@
 # Created by Juan Martinez at 1/27/2020
 Feature: Manages board's list
-  # As a [regular user], It wants [manage lists], so that It [manage lists on board]
-  Background:
-    Given Sets base URI "https://api.trello.com/1"
-    And Sets KEY "keyValue"
-    And Sets TOKEN "tokenValue"
+  As a [regular user], It wants [manage lists], so that It [manage lists on board]
 
   Scenario: # Create a new list on a board
     Given Sets a POST request to "/boards/"
       | key  | value              |
       | name | newBoardPOSTToList |
     And Sends request
-    And Should return status code "200"
+    And Should return status code 200
     And Saves response as "boardObject"
     When Sets a POST request to "/list"
       | key          | value            |
       | name         | newListName      |
       | idBoard      | (boardObject.id) |
     And Sends request
-    Then Should return status code "200"
+    Then Should return status code 200
     And Validates response body
     And Validates schema
     And Sets a DELETE request to "/board/(boardObject.id)"
     And Sends request
-    And Should return status code "200"
+    And Should return status code 200
--- trello/features/steps/ListSmoke.py
+++ trello/features/steps/ListSmoke.py
@@ -2,24 +2,9 @@ from behave import given, when, then, step
 from core.ui.utils.SchemaValidator import SchemaValidator
 
 
-@given('Sets base URI "{url}"')
-def step_impl(context, url):
-    context.url_value = url
-
-
-@step('Sets KEY "{key}"')
-def step_impl(context, key):
-    context.key_value = key
-
-
-@step('Sets TOKEN "{token}"')
-def step_impl(context, token):
-    context.token_value = token
-
-
-@given('Sets a POST request to "{api}"')
-def step_impl(context, api):
-    context.api_value = api
+@given('Sets a POST request to "{endpoint}"')
+def step_impl(context, endpoint):
+    context.api_value = endpoint
     for row_name in context.table:
         print(row_name['key'])
     context.name_value = row_name
@@ -33,9 +18,9 @@ def step_impl(context):
     context.id_value = context.json_response.json()['id']
 
 
-@step('Should return status code "{status_code}"')
-def step_impl(context, status_code):
-    assert context.json_response.status_code is int(status_code)
+@step('Should return status code {number:d}')
+def step_impl(context, number):
+    assert context.json_response.status_code is number
 
 
 @step('Saves response as "{response_object}"')
@@ -43,17 +28,17 @@ def step_impl(context, name_object):
     context.name_object = context.json_response
 
 
-@when('Sets a POST request to "{api}"')
-def step_impl(context, api):
-    context.api_value = api
+@when('Sets a POST request to "{endpoint}"')
+def step_impl(context, endpoint):
+    context.api_value = endpoint
     for row in context.table:
         print(row['key'])
     context.name_value = row
 
 
-@then('Should return status code "{status_code}"')
-def step_impl(context, status_code):
-    assert context.json_response.status_code is int(status_code)
+@then('Should return status code {number:d}')
+def step_impl(context, number):
+    assert context.json_response.status_code is number
 
 
 @step("Validates response body")
@@ -70,8 +55,8 @@ def step_impl(context):
     assert True is expected
 
 
-@step('Sets a DELETE request to "{api}"')
-def step_impl(context, api):
-    context.api = api
-    context.board.delete_board(context.url_value + context.api, context.id_value, context.key_value,
+@step('Sets a DELETE request to "{endpoint}"')
+def step_impl(context, endpoint):
+    context.api_value = endpoint
+    context.board.delete_board(context.url_value + context.api_value, context.id_value, context.key_value,
                                context.token_value)
