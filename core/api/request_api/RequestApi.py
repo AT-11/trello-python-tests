a5212d5fce74d88cd76ed5fd76483a52b9166de3
juanMartinez <juan.martinez@fundacion-jala.org>
2020-01-28 19:17:31 -0400

    Api request first part implemented

--- core/api/request_api/RequestApi.py
+++ core/api/request_api/RequestApi.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 
@@ -6,24 +8,24 @@ class RequestApi(object):
     def __init__(self):
         self.response = ""
 
-    def post_request(self, input_url, input_name, input_key, input_token):
-        querystring = {"name": input_name, "defaultLabels": "true", "defaultLists": "true",
-                       "keepFromSource": "none",
-                       "prefs_permissionLevel": "private", "prefs_voting": "disabled", "prefs_comments": "members",
-                       "prefs_invitations": "members", "prefs_selfJoin": "true", "prefs_cardCovers": "true",
-                       "prefs_background": "blue", "prefs_cardAging": "regular",
-                       "key": input_key,
-                       "token": input_token}
+    def post_request(self, input_url, map_object):
+        values = {}
+        for row in map_object:
+            values.update({row['key']: row['value']})
+        values.update({"key": ""})
+        values.update({"token": ""})
+        querystring = json.dumps(values)
+        url = "https://api.trello.com/1" + input_url
 
         method_type = "POST"
-        self.response = requests.request(method_type, input_url, params=querystring)
+        self.response = requests.request(method_type, url, params=querystring)
         return self.response
 
-    def delete_request(self, input_url, input_id, input_key, input_token):
+    def delete_request(self, input_url, input_id):
         url = input_url + input_id
 
-        querystring = {"key": input_key,
-                       "token": input_token}
+        querystring = {"key": "",
+                       "token": ""}
 
         method_type = "DELETE"
         self.response = self.send_request.method_request(method_type, url, querystring)
--- trello/features/steps/ListSmoke.py
+++ trello/features/steps/ListSmoke.py
@@ -1,25 +1,25 @@
 from behave import given, when, then, step
+
+from core.api.request_api.RequestApi import RequestApi
 from core.utils.SchemaValidator import SchemaValidator
 
 
 @given('Sets a POST request to "{endpoint}"')
 def step_impl(context, endpoint):
     context.endpoint_value = endpoint
-    for row_name in context.table:
-        print(row_name['key'])
-    context.name_value = row_name
-    context.validator = SchemaValidator()
+    context.map_object = context.table
+    context.request_api = RequestApi()
 
 
 @step("Sends request")
 def step_impl(context):
-    context.json_response = context.list.post_board(context.url_value + context.endpoint_value, context.name_value,
-                                                    context.key_value, context.token_value)
+    context.json_response = context.request_api.post_request(context.endpoint_value, context.map_object)
     context.id_value = context.json_response.json()['id']
 
 
 @step('Should return status code {number:d}')
 def step_impl(context, number):
+    context.validator = SchemaValidator()
     assert context.json_response.status_code is number
 
 
