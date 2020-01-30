9f4da3acd20d122492de0a87b64bdd7646d62c0a
Enrique-C <Enrique.Carrizales@fundacion-jala.org>
2020-01-30 17:12:43 -0400

    Update SchemaValidator

--- core/utils/SchemaValidator.py
+++ core/utils/SchemaValidator.py
@@ -1,22 +1,18 @@
 import json
 from jsonschema import validate
 
+from core.utils.JsonFileReader import JsonFileReader
+
 
 class SchemaValidator(object):
 
     # Validates json schema
-    def validate_schema(self, input_value):
+    def validate(self, body_response, json_schema):
+        json_path = 'trello/jsonfiles/' + json_schema
         try:
-            json_schema = {
-                "type": "object",
-                "properties": {
-                    "id": {"type": "string", "pattern": "^[A-Za-z0-9]+$"},
-                    "name": {"type": "string", "pattern": "^[A-Za-z0-9]+$"},
-                    "desc": {"type": "string", "pattern": "^$|^[A-Za-z0-9]+$"},
-                }
-            }
-            my_json = json.loads(input_value)
-            validate(instance=my_json, schema=json_schema)
+            json_schema = JsonFileReader.read(json_path)
+            body_json = json.loads(body_response)
+            validate(instance=body_json, schema=json_schema)
         except:
             return False
         return True
