fb2f8a235b8fa19404b90460bdaf5b8b111f193d
juanMartinez <juan.martinez@fundacion-jala.org>
2020-01-30 17:08:12 -0400

    generation of json query param improved in another method

--- core/api/request_api/RequestApi.py
+++ core/api/request_api/RequestApi.py
@@ -12,26 +12,25 @@ class RequestApi(object):
         self.response = ""
         self.config = EnvironmentConfiguration()
 
-    def check_id(self, value, response_param):
-        key = value[1:value.index(".")]
-        return response_param.get(key, None)
+    def generate_data(self, data_table, response_param):
+        data_dictionary = {}
+        for row in data_table:
+            row_key = row['key']
+            row_value = row['value']
+            if re.search("[.]", row_value):
+                key = row_value[1:row_value.index(".")]
+                row_value = response_param.get(key, None)
+            if re.search("[(|)]", row_value):
+                row_value = self.config.get_config_file()[row_key]
+            data_dictionary[row_key] = row_value
+        data_dictionary["key"] = self.config.get_config_file()['key']
+        data_dictionary["token"] = self.config.get_config_file()['token']
+        return data_dictionary
 
-    def do_request(self, http_type, input_endpoint, table_object, response_param):
-        values = {}
-        for row in table_object:
-            key_value = row['key']
-            value_value = row['value']
-            if re.search("[.]", value_value):
-                response_value = self.check_id(value_value, response_param)
-                value_value = response_value
-            if re.search("[(|)]", value_value):
-                value_value = self.config.get_config_file()[key_value]
-            values[key_value] = value_value
+    def do_request(self, http_type, input_endpoint, data_table, response_param):
+        data = self.generate_data(data_table, response_param)
 
-        values["key"] = self.config.get_config_file()['key']
-        values["token"] = self.config.get_config_file()['token']
-
-        body_content = json.dumps(values)
+        body_content = json.dumps(data)
         url = self.config.get_config_file()['url_trello'] + input_endpoint
         HEADERS = {'content-type': 'application/json'}
         self.response = requests.request(http_type, url, data=body_content, headers=HEADERS)
