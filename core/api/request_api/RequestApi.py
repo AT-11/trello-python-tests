d2a59b5b6edbe5ee0391d5dda1ea6d22564fc73b
Alan Mihael Escalera <alan.escalera@fundacion-jala.org>
2020-02-02 22:38:10 -0400

    implemented-read-variables

--- core/api/request_api/RequestApi.py
+++ core/api/request_api/RequestApi.py
@@ -6,6 +6,13 @@ import requests
 from core.utils.EnvironmentConfiguration import EnvironmentConfiguration
 
 
+def replace_variables(input_endpoint, response_param):
+    for key in response_param:
+        if input_endpoint.find(key) > -1:
+            input_endpoint = input_endpoint.replace(key, response_param[key]).replace(".id", "")
+    return input_endpoint
+
+
 class RequestApi(object):
 
     def __init__(self):
@@ -24,13 +31,16 @@ class RequestApi(object):
 
     def generate_data(self, data_table, response_param):
         data_dictionary = {}
-        for row in data_table:
-            row_key = row['key']
-            row_value = row['value']
-            row_value = self.verify_value(row_key, row_value, response_param)
-            data_dictionary[row_key] = row_value
+        if data_table is not None:
+            for row in data_table:
+                row_key = row['key']
+                row_value = row['value']
+                row_value = self.verify_value(row_key, row_value, response_param)
+                data_dictionary[row_key] = row_value
+
         data_dictionary["key"] = self.config.get_config_file()['key']
         data_dictionary["token"] = self.config.get_config_file()['token']
+
         return data_dictionary
 
     def do_request(self, http_type, input_endpoint, data_table, response_param):
@@ -39,15 +49,19 @@ class RequestApi(object):
         data = self.generate_data(data_table, response_param)
 
         body_content = json.dumps(data)
-        url = self.config.get_config_file()['url_trello'] + input_endpoint
+        url = self.config.get_config_file()['url_trello'] + replace_variables(input_endpoint, response_param)
         HEADERS = {'content-type': 'application/json'}
-        if http_type == "POST":
+        # if http_type == "POST":
+        if data_table is not None:
             self.response = requests.request(http_type, url, data=body_content, headers=HEADERS)
         else:
-            values["key"] = self.config.get_config_file()['key']
-            values["token"] = self.config.get_config_file()['token']
-            url += self.verify_value_delete(response_param)
-            self.response = requests.request(http_type, url, params=values)
+            self.response = requests.request(http_type, url, params=body_content, headers=HEADERS)
+
+        # else:
+        #     values["key"] = self.config.get_config_file()['key']
+        #     values["token"] = self.config.get_config_file()['token']
+        #     url += self.verify_value_delete(response_param)
+        #     self.response = requests.request(http_type, url, params=values)
         return self.response
 
     def verify_value_delete(self, response_param):
