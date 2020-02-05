33448ecffc344752522af5e81f6314aca869d87b
juanMartinez <juan.martinez@fundacion-jala.org>
2020-02-05 16:02:16 -0400

    observacions fixed

--- trello/api/features/environment.py
+++ trello/api/features/environment.py
@@ -5,8 +5,9 @@ from core.api.features.steps.Common import *
 def before_scenario(context, scenario):
     context.id_dictionary = {}
     context.request_api = RequestApi()
-    print("START: %s", scenario)
 
 
 def after_scenario(context, scenario):
-    print("END: %s", scenario)
+    """
+    To do
+    """
