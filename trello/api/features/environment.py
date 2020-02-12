aafe84d62054007b567dfc26bf22421045c7682f
juanMartinez <juan.martinez@fundacion-jala.org>
2020-02-12 16:01:37 -0400

    allure implemented and implemented in travis

--- trello/api/features/environment.py
+++ trello/api/features/environment.py
@@ -1,6 +1,2 @@
 # Here we are importing Functions from core
 from core.api.features.Functions import *
-
-from allure_behave.hooks import allure_report
-
-allure_report("C:/Users/Admin/Documents/API Testing/trello-python-tests/reports")
